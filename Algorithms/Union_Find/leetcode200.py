05f252ad0b9ba77b78bafe9b0793decec040a5a8
Yaqiao Liu <liu.yaqi@husky.neu.edu>
2018-12-04 11:38:25 +0800

    Update leetcode200.py

--- Algorithms/Union_Find/leetcode200.py
+++ Algorithms/Union_Find/leetcode200.py
@@ -20,4 +20,5 @@ Input:
 
 Output: 3
 '''
-常见的BFS和DFS算法见 [leetcode200](https://github.com/liuyaqiao/Algorithms/blob/master/src)
+常见的BFS和DFS算法见
+[leetcode200](https://github.com/liuyaqiao/Algorithms/tree/master/src/200.py) 
